d1974b5e2be7ca6bb502ef5a34812435b6773057
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-30 11:59:00 -0700

    Rename getweather.js file for specificity

--- src/getcurrentweather.js
+++ src/getcurrentweather.js
@@ -1,4 +1,4 @@
-export const getWeather = async (str) => {
+export const getCurrentWeather = async (str) => {
   const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${str}/?key=CSDFLBZZSWCSSEZDFPQ22QWZD`, {mode: 'cors'});
   const weatherData = await response.json();
   console.log(weatherData);
