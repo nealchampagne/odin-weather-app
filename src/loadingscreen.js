6fa0ed8075f50bbcc884d99f1decd4a6241f641a
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-09-05 13:14:54 -0700

    Add loading screen while waiting for data

---
+++ src/loadingscreen.js
@@ -0,0 +1,16 @@
+import { clearChildren } from "./clearchildren"
+
+// Display a loading message while waiting for data to come back from fetch
+export const loadingScreen = () => {
+
+  const content = document.getElementById('content');
+  const loadingMessage = document.createElement('div');
+
+  clearChildren(content);
+
+  loadingMessage.classList.add('loadingmessage');
+
+  loadingMessage.textContent = 'Loading...';
+
+  content.appendChild(loadingMessage);
+};
