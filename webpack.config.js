8820382dc10a35cad8edea8b5a72fa7a839bab49
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-09-30 10:56:15 -0700

    Update HTML title in webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      title: "To-Do",
+      title: "Odin-Weather",
     }),
     new MiniCssExtractPlugin(),
   ],
